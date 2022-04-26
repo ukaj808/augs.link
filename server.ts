3645da3f98cf2616be2c45d8cf9149150bf4ff60
ukaj808 <ukaj808@gmail.com>
2022-04-26 00:12:42 -0400

    1. Added Room Page + Style 2. Updated Styles + HTML + JS for the Index

--- server.ts
+++ server.ts
@@ -2,31 +2,36 @@ import {serve} from "https://deno.land/std@0.136.0/http/server.ts";
 import {fromFileUrl} from "https://deno.land/std@0.136.0/path/mod.ts";
 import {readableStreamFromReader} from "https://deno.land/std@0.136.0/streams/conversion.ts";
 
-async function handler(req: Request): Promise<Response> {
-    console.log("Method:", req.method);
-    console.log("Headers:", req.headers);
-
-    const url = new URL(req.url);
-
-    console.log("Path:", url.pathname);
-    console.log("Query parameters:", url.searchParams);
-
-    const indexPageUrl = new URL("./index.html", import.meta.url);
-    const notFoundPageUrl = new URL("./pages/not_found.html", import.meta.url)
-
-    if (req.method === "GET" && url.pathname === "/") {
-        const indexPageFile = await Deno.open(fromFileUrl(indexPageUrl));
-        return new Response(readableStreamFromReader(indexPageFile), {
-            status: 200,
-            headers: {"content-type": "text/html",},
-        });
-    }
-
-    const notFoundPageFile = await Deno.open(fromFileUrl(notFoundPageUrl));
-    return new Response(readableStreamFromReader(notFoundPageFile), {
-        status: 404,
-        headers: {"content-type": "text/html",},
+async function generateResponse(path: string, status: number, contentType: string): Promise<Response> {
+    const file = await Deno.open(fromFileUrl(new URL(path, import.meta.url)));
+    return new Response(readableStreamFromReader(file), {
+        status: status,
+        headers: {"content-type": contentType,},
     });
 }
 
-serve(handler);
+function handle(req: Request): Promise<Response> {
+    const requestUrl = new URL(req.url);
+
+    if (req.method === "GET") {
+        if (requestUrl.pathname === "/") {
+            return generateResponse("./pages/index.html", 200, "text/html");
+        } else if (requestUrl.pathname === "/scripts/index.js") {
+            return generateResponse("./scripts/index.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/styles/index.css") {
+            return generateResponse("./styles/index.css", 200, "text/css");
+        } else if (requestUrl.pathname === "/scripts/room.js") {
+            return generateResponse("./scripts/room.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/styles/room.css") {
+            return generateResponse("./styles/room.css", 200, "text/css");
+        }
+    } else if (req.method === "POST") {
+        if (requestUrl.pathname === "/create-room") {
+            return generateResponse("./pages/room.html", 201, "text/html");
+        }
+    }
+
+    return generateResponse("./pages/not_found.html", 404, "text/html");
+}
+
+serve(handle);
