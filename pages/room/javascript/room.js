08bfdf6ec573a226ee232ead88260bc276451d12
ukaj808 <ukaj808@gmail.com>
2022-04-29 12:57:25 -0400

    1. Create web component sections

---
+++ pages/room/javascript/room.js
@@ -0,0 +1,19 @@
+import {OrderSection} from "./web-components/sections/order_section.js";
+customElements.define('order-section', OrderSection);
+
+import {CurrentSection} from "./web-components/sections/current_section.js";
+customElements.define('current-section', CurrentSection);
+
+import {DemocracySection} from "./web-components/sections/democracy_section.js";
+customElements.define('democracy-section', DemocracySection);
+
+import {DropSection} from "./web-components/sections/drop_section.js";
+customElements.define('drop-section', DropSection);
+
+import {QueueSection} from "./web-components/sections/queue_section.js";
+customElements.define('queue-section', QueueSection);
+
+
+
+
+
--- server.ts
+++ server.ts
@@ -14,24 +14,47 @@ function handle(req: Request): Promise<Response> {
     const requestUrl = new URL(req.url);
 
     if (req.method === "GET") {
+        // Index Content
         if (requestUrl.pathname === "/") {
-            return generateResponse("./pages/index.html", 200, "text/html");
-        } else if (requestUrl.pathname === "/scripts/index.js") {
-            return generateResponse("./scripts/index.js", 200, "application/javascript");
-        } else if (requestUrl.pathname === "/styles/index.css") {
-            return generateResponse("./styles/index.css", 200, "text/css");
-        } else if (requestUrl.pathname === "/scripts/room.js") {
-            return generateResponse("./scripts/room.js", 200, "application/javascript");
-        } else if (requestUrl.pathname === "/styles/room.css") {
-            return generateResponse("./styles/room.css", 200, "text/css");
+            return generateResponse("./pages/home/html/index.html", 200, "text/html");
+        } else if (requestUrl.pathname === "/pages/home/javascript/index.js") {
+            return generateResponse("./pages/home/javascript/index.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/home/css/index.css") {
+            return generateResponse("./pages/home/css/index.css", 200, "text/css");
         }
+
+        // Room Content
+        else if (requestUrl.pathname === "/pages/room/javascript/room.js") {
+            return generateResponse("./pages/room/javascript/room.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/room/css/room.css") {
+            return generateResponse("./pages/room/css/room.css", 200, "text/css");
+        }
+
+        // Web Component Sections used in Room
+        else if (requestUrl.pathname === "/pages/room/javascript/web-components/sections/current_section.js") {
+            return generateResponse("./pages/room/javascript/web-components/sections/current_section.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/room/javascript/web-components/sections/democracy_section.js") {
+            return generateResponse("./pages/room/javascript/web-components/sections/democracy_section.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/room/javascript/web-components/sections/drop_section.js") {
+            return generateResponse("./pages/room/javascript/web-components/sections/drop_section.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/room/javascript/web-components/sections/order_section.js") {
+            return generateResponse("./pages/room/javascript/web-components/sections/order_section.js", 200, "application/javascript");
+        } else if (requestUrl.pathname === "/pages/room/javascript/web-components/sections/queue_section.js") {
+            return generateResponse("./pages/room/javascript/web-components/sections/queue_section.js", 200, "application/javascript");
+        }
+
+        // No Content Found...
+        return generateResponse("./pages/404/html/404.html", 404, "text/html");
+
     } else if (req.method === "POST") {
+        // Create room + navigate to room page
         if (requestUrl.pathname === "/create-room") {
-            return generateResponse("./pages/room.html", 201, "text/html");
+            return generateResponse("./pages/room/html/room.html", 201, "text/html");
         }
     }
 
-    return generateResponse("./pages/not_found.html", 404, "text/html");
+    // Unrecognized Request
+    return generateResponse("./pages/405/html/405.html", 405, "text/html");
 }
 
 serve(handle);
